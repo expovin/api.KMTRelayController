a1002767a1d0ae719a2fb84b3eb50ae24a127e81
Vincenzo Esposito <expovin@gmail.com>
2019-02-17 15:10:44 +0100

    app.js fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var relayRouter = require('../router/relay');
+var relayRouter = require('./routes/relay');
 
 var app = express();
 
