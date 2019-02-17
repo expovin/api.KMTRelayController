2cb63cc3dcaedc8f006d7a96351ea7e86cbda7d5
Vincenzo Esposito <expovin@gmail.com>
2019-02-17 21:00:54 +0100

    Added collateral methods

--- routes/kmt.js
+++ routes/kmt.js
@@ -6,25 +6,52 @@ const router = express.Router();
 const KMT = new KMTClass()
 
 /* GET users listing. */
-router.route('/relay/:relay')
+
+router.route('/relay/')
 .get( function(req, res, next) {
-   res.status(200).json({result:'KO', data: "Getting status for relay "+req.params.relay});
+   // Get Board Status (ENABLD or DISABLED)
 })
 .post( function(req, res, next) {
-    // res.status(200).json({result:'KO', data: MarketTrends.getMarketTrends()});
+   // Enable Board
+   KMT.enableBoard()
+   .then( result => res.status(200).json(result),
+           error => res.status(200).json(error))
+})
+.delete( function(req, res, next) {
+   // Disable Board
+   KMT.disabledBoard()
+   .then( result => res.status(200).json(result),
+           error => res.status(200).json(error))
+})
+
+router.route('/relay/:relay')
+.get( function(req, res, next) {
+   // GET the Relay status
+})
+.post( function(req, res, next) {
+   KMT.enableRelay(req.params.relay, req.body.TTC)
+   .then( result => res.status(200).json(result),
+           error => res.status(200).json(error))   
  })
  .delete( function(req, res, next) {
-    // res.status(200).json({result:'KO', data: MarketTrends.getMarketTrends()});
+    KMT.disableRelay(req.params.relay)
+    .then( result => res.status(200).json(result),
+           error => res.status(200).json(error))    
  })
 
 
 router.route('/ports')
 .get( function(req, res, next) {
     KMT.listPort()
-    .then(  ports => res.status(200).json({result:'success', data: ports}), 
-            error => res.status(500).json({result:'failed', error: error})
+    .then(  ports => res.status(200).json({success:true, data: ports}), 
+            error => res.status(500).json({success:false, error: error})
     )
 })
 
+router.route('/status')
+.get( function(req, res, next) {
+   // Get connection status
+   res.status(200).json({success:true, data: KMT.getConnectionStatus()});
+})
 
 module.exports = router;
