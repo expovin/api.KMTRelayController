18fd74d40ce1360b026c815e9b3e3e77d26dec8b
Vincenzo Esposito <expovin@gmail.com>
2019-02-17 18:36:19 +0100

    Router KMT

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var relayRouter = require('./routes/relay');
+var KMTRouter = require('./routes/kmt');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/KMT', relayRouter);
+app.use('/KMT', KMTRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
