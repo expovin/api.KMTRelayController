a3060a148ab8705b2ebc8bdc24de92978de6aff4
Vincenzo Esposito <expovin@gmail.com>
2019-02-17 15:06:31 +0100

    Router

---
+++ routes/relay.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const KMTClass = require('../lib/KMTClass');
+const router = express.Router();
+
+
+const KMT = new KMTClass()
+
+/* GET users listing. */
+router.route('/:relay')
+.get( function(req, res, next) {
+   // res.status(200).json({result:'KO', data: MarketTrends.getMarketTrends()});
+})
+.post( function(req, res, next) {
+    // res.status(200).json({result:'KO', data: MarketTrends.getMarketTrends()});
+ })
+ .delete( function(req, res, next) {
+    // res.status(200).json({result:'KO', data: MarketTrends.getMarketTrends()});
+ })
+
+
+router.route('/ports')
+.get( function(req, res, next) {
+    KMT.listPort()
+    .then(  ports => res.status(200).json({result:'success', data: ports}), 
+            error => res.status(500).json({result:'failed', error: error})
+    )
+})
+
+
+module.exports = router;
